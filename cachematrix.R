ce97b13d876d9817dcbad5b11cbe3fe61c484dde
adityachittor <aditya.chittor@gmail.com>
2015-03-22 17:19:16 -0500

    Full functionality realized

--- cachematrix.R
+++ cachematrix.R
@@ -3,31 +3,37 @@
 ## be called thus realizing efficiency
 ## Two functions will realize this logic
 ## Function makeCacheMatrix will create a special object
-## Function cacheSolve will calculate the inverse
+## Function cacheSolve will calculate the inverse of the matrix presented to 
 
+
+
+## First function
+## An object will enclose a matrix, its inverse and utility functions
+## to allow calculations
 
 makeCacheMatrix  <- function(x = matrix()) {
 
-        ## initialze a matrix to hold an inverse
+        ## initialze a matrix to hold inverse of matrix
         
         inx <- matrix()
         
-        ## set values to hold matrix and its inverse
+        ## Initiallize variable to hold matrix and its inverse
+        ## in this function
         
         set <- function(y) {
                 x <<- y
                 inx <<- NULL                
         }
         
-        ## get x when needed
+        ## Below function returns the non inversed matrix
         
         get <- function() x 
         
-        ## set inverse of function
+        ## Set value to the variable holding inverse of matrix
         
         setinverse <- function(inverse) inx <<- inverse
         
-        ## calculate inverse of function
+        ## returns the variable of the matrix
         
         getinverse <- function() inx
         
@@ -37,14 +43,25 @@ makeCacheMatrix  <- function(x = matrix()) {
 }
 
 
-## below function will calculate inverse of a matrix in an efficient manner
+## Second function will calculate inverse of a matrix only if it is not
+## already calculated
 
 cacheSolve <- function(x, ...) {
         
+        ## get value of variable holding inverse of matrix
+        
         i <- x$getinverse()
         
-        if(is.null(i))
+        ## check if inverse is already calculated
+        ## if inverse is not calculated, the initialized 1 by 1 NA matrix will
+        ## be present
+        ## 
+        if (nrow(i)==1&&ncol(i)==1&&is.na(i[1,1])) 
+        {}
+        else
         {
+                ## Inverse is already available
+                ## return already calculated 
                 message ("getting catched data")
                 return(i)
         }
